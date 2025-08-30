b8b12cbdb215836ad7d050302ed7e4eb76a126a2
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-08-30 23:17:37 +0100

    Add OrganizerGuard to activate ticket endpoint and create global type definition file

--- src/routes/activate/activate.controller.ts
+++ src/routes/activate/activate.controller.ts
@@ -1,12 +1,14 @@
-import { Request } from 'express';
+import { UseGuards } from '@nestjs/common';
 import { ActivateService } from './activate.service';
 import { Controller, Get, Param, Req } from '@nestjs/common';
+import { OrganizerGuard } from '../auth/Guards/organizer.guard';
 
 @Controller('activate')
 export class ActivateController {
   constructor(private readonly activateService: ActivateService) {}
 
   @Get('ticket/:id')
+  @UseGuards(OrganizerGuard)
   activate(
     @Param('id') id: string,
     @Param('eventId') eventId: string,
